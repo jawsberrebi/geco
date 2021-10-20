e230890232ae0ef1c517e934e613cbcbcff0074e
jawsberrebi <berjaws@gmail.com>
2021-10-20 11:57:58 +0200

    Envoi page de connexion.

--- connexion.php
+++ connexion.php
@@ -1,33 +1,38 @@
 <html>
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
-    <div id="container">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
 
-        <form action="verification.php" method="POST">
-            <h1>Connexion</h1>
+        <img src="images/image-vitrine.jpg" alt="image de description" id="image_connexion" />
 
-            <label>
-                <b>Nom d'utilisateur</b>
-            </label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required />
+        <div id="box">
 
-            <label>
-                <b>Mot de passe</b>
-            </label>
-            <input type="password" placeholder="Entrer le mot de passe" name="password" required />
+            
 
-            <input type="submit" id='submit' value='LOGIN' />
-            <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-            ?>
-        </form>
-    </div>
-</body>
+            <form action="verification.php" method="POST">
+                <h1 id="title_form">Connexion</h1>
+
+                <input type="text" placeholder="Email ou nom d'utilisateur" name="username" required />
+
+                <input type="password" placeholder="Mot de passe" name="password" required />
+
+                <input type="submit" id='submit' value='Connexion' />
+
+                <a href="" id="password_lost">Mot de passe oublié ?</a>
+
+                <p id="indication">Si vous n'avez pas de compte, veuillez contacter votre médecin.</p>
+
+                <?php
+                    if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1 || $err==2)
+                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    }
+                ?>
+
+            </form>
+        </div>
+    </body>
 </html>
