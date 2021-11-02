7266164371b12c69c8219b0fa78e9fdc373b65aa
jawsberrebi <berjaws@gmail.com>
2021-11-02 17:34:57 +0100

    Add files via upload

---
+++ geco-BDD/backend/fenetre_modale_tableau_de_bord.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8" />
+
+<a href="modifier_mon_compte.php" id="modification_compte">Modifier mon compte</a><br />
+
+<a href="deconnexion.php" id="deconnexion">Déconnexion</a><br />
