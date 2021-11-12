5b57c36aa1d52d32928f6ef4e3e3109e38acf809
jawsberrebi <berjaws@gmail.com>
2021-11-12 18:32:47 +0100

    supres

--- fenetre_modale_tableau_de_bord.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8" />
-
-<a href="modifier_mon_compte.php" id="modification_compte">Modifier mon compte</a><br />
-
-<a href="deconnexion.php" id="deconnexion">Déconnexion</a><br />
